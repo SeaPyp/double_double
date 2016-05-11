810872594fb3d7db74ec38df8c6443b379b3ee7f
Sean Pypers <seapyp@gmail.com>
2016-05-11 16:40:22 -0700

    adds factory stuff

--- js/todosFactory.js
+++ js/todosFactory.js
@@ -4,7 +4,24 @@ angular.module('TodoApp')
 TodoFactory.$inject = ['$http'];
 
 function TodoFactory($http){
+  var todoUrl = 'http://localhost:3000/api/todos/';
   var todoFactory = {};
+
+  todoFactory.getAllTasks = function(){
+    return $http.get(todoUrl);
+  };
+
+  todoFactory.updateOne = function(id, data){
+    return $http.patch(todoUrl + id, data);
+  };
+
+  todoFactory.new = function(data){
+    return $http.post(todoUrl, data);
+  }
+
+  todoFactory.destroy = function(id){
+    return
+  }
 
   return todoFactory;
 }
